733020177046368e9e1ebee60244113e40dc82d6
mobadran <mohamedbadran1492008@gmail.com>
2026-03-30 20:44:05 +0200

    tablet responsive

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -57,7 +57,7 @@
                                         @csrf
                                         @method('PATCH')
                                         <input type="hidden" name="id" value="{{ $id }}">
-                                        <input type="number" name="quantity" value="{{ $details['quantity'] }}" class="w-20 bg-gray-50 border border-gray-200 rounded-lg px-3 py-2 text-center focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all font-medium" min="1">
+                                        <input type="number" name="quantity" value="{{ $details['quantity'] }}" class="w-14 bg-gray-50 border border-gray-200 rounded-lg px-1 py-2 text-center focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all font-medium" min="1">
                                         <button type="submit" class="text-blue-600 hover:text-blue-800 p-2 rounded-lg bg-blue-50 hover:bg-blue-100 transition-colors shrink-0 tooltip" title="Update Quantity">
                                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
                                         </button>
